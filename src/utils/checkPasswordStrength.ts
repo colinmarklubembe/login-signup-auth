8adc7c1bcd32f2c063585383f5ba032ba5e139a5
colinmarklubembe <107845562+colinmarklubembe@users.noreply.github.com>
2024-10-02 20:40:07 +0300

    refactor: Update password strength validation with zxcvbn-ts library

--- src/utils/checkPasswordStrength.ts
+++ src/utils/checkPasswordStrength.ts
@@ -1,11 +1,38 @@
-import zxcvbn from "zxcvbn";
+import { zxcvbn, zxcvbnOptions } from "@zxcvbn-ts/core";
+import * as zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
+import * as zxcvbnEnPackage from "@zxcvbn-ts/language-en";
 
 const validatePasswordStrength = (password: string) => {
   const minLength = 8;
+
+  // Set up options
+  const options = {
+    translations: zxcvbnEnPackage.translations,
+    dictionary: {
+      ...zxcvbnCommonPackage.dictionary,
+      ...zxcvbnEnPackage.dictionary,
+    },
+    graphs: zxcvbnCommonPackage.adjacencyGraphs,
+  };
+
+  zxcvbnOptions.setOptions(options);
+
   const result = zxcvbn(password);
-  const passwordStrength = password.length >= minLength && result.score >= 3;
+
+  const minScore = Math.max(2, result.score);
+
+  const hasSufficientLength = password.length >= minLength;
+
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const hasDigit = /\d/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+
+  const passwordStrength =
+    hasSufficientLength &&
+    minScore <= result.score &&
+    hasSpecialChar &&
+    hasDigit &&
+    hasLowercase;
 
   return passwordStrength;
 };
-
-export default { validatePasswordStrength };
